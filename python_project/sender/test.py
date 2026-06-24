e8ac6250d874cb1ee8242a9ff5e724ca165dbb0f
goat_element <istamenov99@gmail.com>
2026-06-24 14:28:12 +0200

    test.py dodato

---
+++ python_project/sender/test.py
@@ -0,0 +1,9 @@
+from prstenovi_kljuceva import *
+
+lista_kljuceva = load_prsten_javnih_kljuceva()
+
+print(lista_kljuceva)
+
+#ukloni_javni_kljuc("AABBCCDD11223344")
+
+print(dohvati_javni_kljuc("AABBCCDD11223344"))
