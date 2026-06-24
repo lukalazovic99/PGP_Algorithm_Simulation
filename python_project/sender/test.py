7755675f6266493d9ce68bdc63c293f20a2db727
goat_element <istamenov99@gmail.com>
2026-06-24 14:34:55 +0200

    test.py poboljsano

--- python_project/sender/test.py
+++ python_project/sender/test.py
@@ -2,8 +2,23 @@ from prstenovi_kljuceva import *
 
 lista_kljuceva = load_prsten_javnih_kljuceva()
 
+novikljuc = {
+        "timestamp": "2026-06-23 14:30:00",
+        "key_id": "MOJID",
+        "user_id": "Bogdan bogdanNBA@vranje.vr",
+        "key_size": "2048",
+        "public_key": "-----BEGIN RSA PUBLIC KEY-----FAKEKEYDATA-----END RSA PUBLIC KEY-----"
+    }
 print(lista_kljuceva)
 
-#ukloni_javni_kljuc("AABBCCDD11223344")
+dodaj_javni_kljuc(novikljuc)#ovo snimi ali lista_kjuceva nije updated
 
-print(dohvati_javni_kljuc("AABBCCDD11223344"))
+lista_kljuceva = load_prsten_javnih_kljuceva()
+
+print(lista_kljuceva)
+
+print(dohvati_javni_kljuc("MOJID"))
+
+ukloni_javni_kljuc("MOJID")
+
+print(dohvati_javni_kljuc("MOJID"))
