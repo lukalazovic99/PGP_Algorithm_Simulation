90141c6f6584cf4fd1bf84ab0fe4b6d8dead8ebe
lukalazovic99 <luka.lazovic7@gmail.com>
2026-07-06 22:29:23 +0200

    receiver main decryption body

---
+++ python_project/receiver/decryption.py
@@ -0,0 +1,7 @@
+
+
+def decrypt_session_key(encrypted_session_key_b64: str, private_key_pem: str) -> bytes:
+    pass
+
+def decrypt_payload(ciphertext_b64: str, session_key: bytes, iv_b64: str, algorithm: str) -> bytes:
+    pass
--- python_project/receiver/r64_zip.py
+++
@@ -1,8 +0,0 @@
-import base64
-import zlib
-
-def unzip(message):
-    return zlib.decompress(message)
-
-def decode_base64(message):
-    return base64.b64decode(message)
